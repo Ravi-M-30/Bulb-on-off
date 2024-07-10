acdaf03a0fc4e5e83716cb496e4f1da466a638ed
Ravi-M-30 <mraviravi709@gmail.com>
2024-07-10 15:40:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var On_button=document.getElementById("On");
+var Off_button=document.getElementById("Off")
+var image=document.getElementById("Bulb")
+
+function Bulb_On() {
+    image.src="./image/bulb_on.jpg"
+}
+
+function Bulb_Off() {
+    image.src="./image/bulb_off.jpg"
+}
